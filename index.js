4b400ced2b7bc0b27444155cd2047c60c40f511d
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-01-05 17:28:15 +0000

    Added function to generate new activity from API

---
+++ index.js
@@ -0,0 +1,20 @@
+const refreshBtn = document.getElementById('refresh-btn');
+refreshBtn.addEventListener('click', generateActivity);
+const text = document.getElementById('activity-text');
+
+function generateActivity(){
+    text.textContent = "Finding you something to do..."
+    fetch("https://www.boredapi.com/api/activity/")
+        .then(response => response.json())
+        .then(data => {
+            const type = data.type;
+            let classes = document.getElementById('container').classList;
+            if(classes.length>=1){
+                document.getElementById('container').classList.remove(classes[0])
+            }
+            console.log(classes[0])
+            document.getElementById('container').classList.add(type)
+            text.textContent = data.activity;
+            console.log(data)
+        })
+}
