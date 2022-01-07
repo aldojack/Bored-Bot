5e377e1bb4b2aa66cfdc8739ed748e162446942f
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-01-07 23:21:07 +0000

    Now fully changes theme and heading  to match the activity type

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const refreshBtn = document.getElementById('refresh-btn');
 refreshBtn.addEventListener('click', generateActivity);
 const text = document.getElementById('activity-text');
+const heading = document.getElementById('bot-heading');
+let activeClass = "";
+
 
 function generateActivity(){
     text.textContent = "Finding you something to do..."
@@ -8,12 +11,58 @@ function generateActivity(){
         .then(response => response.json())
         .then(data => {
             const type = data.type;
+
             let classes = document.getElementById('container').classList;
-            if(classes.length>=1){
-                document.getElementById('container').classList.remove(classes[0])
+
+            if(classes.length === 0){
+                document.getElementById('container').classList.add(type)
             }
-            document.getElementById('container').classList.add(type)
+            else{
+                document.getElementById('container').classList.replace(activeClass,type)
+            }
+            
+            switch(type){
+                case "education": 
+                    heading.textContent = "👩‍🎓Education Bot👩‍🎓";
+                    break;
+
+                case "recreational":
+                    heading.textContent = "⚡ GET IT DONE Bot⚡";
+                    break;
+
+                case "social":
+                    heading.textContent = "🦜Social Bot🗣";
+                    break;
+                        
+                case "diy":
+                    heading.textContent = "🔨Fix It Bot🔧";
+                    break;
+
+                case "charity":
+                    heading.textContent = "❤Kind Bot❤";
+                    break;
+
+                case "cooking": 
+                    heading.textContent = "🍳Ramsay Bot🥓";
+                    break;
+
+                case "relaxation":
+                    heading.textContent = "☮ Zen Bot ✌";
+                    break;
+
+                case "music":
+                    heading.textContent = "🎵Drop a beat Bot🪕";
+                    break;
+                        
+                case "busywork":
+                    heading.textContent = "🐝Busy Bee Bot🐝";
+                    break;
+                
+                default:
+                    break;
+
+            }
+            activeClass = type;
             text.textContent = data.activity;
-            console.log(data)
         })
 }
